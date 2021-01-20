8cc90f540a24ed6bb31cbfe66c1e59ff7a9d2bce
lizzymejia <72759970+lizzymejia@users.noreply.github.com>
2021-01-20 10:55:47 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 while True:
     if input.sound_level() > 20 and input.light_level() > 15:
         light.set_all(light.rgb(50,150,300)) 
-        
+        music.ba_ding.play_until_done()
+    elif input.sound_level() < 25 and input.light_level() < 20:
+        light.clear()
+        music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 while (true) {
     if (input.soundLevel() > 20 && input.lightLevel() > 15) {
         light.setAll(light.rgb(50, 150, 300))
+        music.baDing.playUntilDone()
+    } else if (input.soundLevel() < 25 && input.lightLevel() < 20) {
+        light.clear()
+        music.stopAllSounds()
     }
     
 }
